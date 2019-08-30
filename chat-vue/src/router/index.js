8232fe64385d017cdd804cb2f2b2c3adcbcad413
gda2048 <goncharovdma@gmail.com>
2019-08-30 11:49:30 +0300

    add login on vue

--- chat-vue/src/router/index.js
+++ chat-vue/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -8,8 +9,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
+
