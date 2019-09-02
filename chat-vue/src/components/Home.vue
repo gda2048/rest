9243920b159c3a2087410377769f3295f06736f9
gda2048 <goncharovdma@gmail.com>
2019-09-02 19:05:36 +0300

    add room.vue and logout

--- chat-vue/src/components/Home.vue
+++ chat-vue/src/components/Home.vue
@@ -1,16 +1,34 @@
 <template>
   <div>
     <h1>Чат на vue.js</h1>
-    <button @click="goLogin">Вход</button>
+    <button v-if='!auth' @click="goLogin">Вход</button>
+    <button v-if='auth' @click="goLogout">Выход</button>
+    <Room v-if="auth"></Room>
   </div>
 </template>
 
 <script>
+  import Room from '@/components/Room'
+
   export default {
     name: "Home",
+    components: {
+      Room
+    },
+    computed: {
+      auth() {
+        if (sessionStorage.getItem("auth_token")) {
+          return true;
+        }
+      }
+    },
     methods: {
-      goLogin(){
+      goLogin() {
         this.$router.push({name: "login"})
+      },
+      goLogout(){
+        sessionStorage.removeItem("auth_token");
+        window.location = '/';
       }
     }
   }
---
+++ chat-vue/src/components/Room.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <ul>
+      <li v-for='room in rooms'>
+        <h3>{{room.creator.username}}</h3>
+        {{room.date}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import $ from 'jquery'
+
+  export default {
+    name: "Room",
+    data() {
+      return {
+        rooms: '',
+      }
+    },
+
+    created() {
+      $.ajaxSetup(
+        {
+          headers: {'Authorization': "Token " + sessionStorage.getItem("auth_token")}
+        }
+      );
+      this.loadRoom();
+
+    },
+    methods: {
+      loadRoom() {
+        $.ajax({
+          url: "http://127.0.0.1:8090/api/v1/chat/room",
+          type: 'GET',
+          success: (response) => {
+            this.rooms = response.data.data;
+            console.log(response)
+          }
+        })
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
